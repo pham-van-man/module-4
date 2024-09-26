1ad612ca7543bc51aea78f2a083b73a9b18fdcc2
pham-van-man <phamvanman.com.vn@gmail.com>
2024-09-26 11:24:24 +0700

    first commit

--- ss8/bai_tap_2/src/main/java/org/example/bai_tap_2/DTO/SongDTO.java
+++ ss8/bai_tap_2/src/main/java/org/example/bai_tap_2/DTO/SongDTO.java
@@ -1,5 +1,8 @@
 package org.example.bai_tap_2.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +11,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SongDTO {
+    @NotBlank(message = "Bài hát không được để trống")
+    @Size(min = 1, max = 800 ,message = "Bài hát có ít nhất 1 kí tự, tối đa 800 kí tự")
+    @Pattern(regexp = "^[0-9À-ỹ]+$",message = "Bài hát không đúng định dạng")
     private String title;
+    @NotBlank(message = "Tác giả không được để trống")
+    @Size(min = 1, max = 300, message = "Tác giả có ít nhât 1 kí tự, tối đa 300 kí tự")
+    @Pattern(regexp = "^[À-ỹ]+$", message = "Tác giả không đúng định dạng")
     private String artist;
+    @NotBlank(message = "Thể loại không được để trông")
+    @Size(min = 1, max = 1000, message = "Thể loại có ít nhất 1 kí tự, tối đa 1000 kí tự")
+    @Pattern(regexp = "^[0-9À-ỹ,]+$", message = "Thể loại không đúng định dạng")
     private String genre;
 }
--- ss8/bai_tap_2/src/main/java/org/example/bai_tap_2/model/Song.java
+++ ss8/bai_tap_2/src/main/java/org/example/bai_tap_2/model/Song.java
@@ -4,9 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,16 +16,7 @@ public class Song {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "Bài hát không được để trống")
-    @Size(min = 1, max = 800 ,message = "Bài hát có ít nhất 1 kí tự, tối đa 800 kí tự")
-    @Pattern(regexp = "^[0-9À-ỹ]+$",message = "Bài hát không đúng định dạng")
     private String title;
-    @NotBlank(message = "Tác giả không được để trống")
-    @Size(min = 1, max = 300, message = "Tác giả có ít nhât 1 kí tự, tối đa 300 kí tự")
-    @Pattern(regexp = "^[À-ỹ]+$", message = "Tác giả không đúng định dạng")
     private String artist;
-    @NotBlank(message = "Thể loại không được để trông")
-    @Size(min = 1, max = 1000, message = "Thể loại có ít nhất 1 kí tự, tối đa 1000 kí tự")
-    @Pattern(regexp = "^[0-9À-ỹ,]+$", message = "Thể loại không đúng định dạng")
     private String genre;
 }
